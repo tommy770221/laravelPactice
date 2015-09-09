8ad633ea07f29d4a9db81fd437b63e9856a20329
Tommy770221 <tommy770221@gmail.com>
2015-09-09 22:23:47 +0800

    add a view to show

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 For Test</div>
+                <div class="title">about</div>
             </div>
         </div>
     </body>
